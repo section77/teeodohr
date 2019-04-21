ad3e1bf9f2961f446ccbef6cc188172a8a4c56f5
Justin Otherguy <justin@justinotherguy.org>
2019-04-21 08:58:37 +0200

    add Bayern3 "Solang der alte Peter..."

--- Software/Teeodohr/Teeodohr.ino
+++ Software/Teeodohr/Teeodohr.ino
@@ -12,6 +12,12 @@ int max_Winkel=70;
 int min_Winkel=20;
 
 // notes in the melody:
+//int melody_1[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+//int noteDurations_1[] = {4, 8, 8, 4, 4, 4, 4, 4};
+
+//alternative melody
+// notes in the melody:
 int melody_1[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations_1[] = {4, 8, 8, 4, 4, 4, 4, 4};
